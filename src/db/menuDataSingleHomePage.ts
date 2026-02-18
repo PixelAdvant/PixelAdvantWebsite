24a04332b7187f1b01c70f373e762cd2b11fea1b
rahson786 <sonu.rahul4all@gmail.com>
2026-02-18 13:43:14 +0530

    feat: Simplify header navigation menu to About, Solutions, Models, Products, Blog

--- src/db/menuDataSingleHomePage.ts
+++ src/db/menuDataSingleHomePage.ts
@@ -2,58 +2,20 @@ import { MenuItemDataType } from "./menuData";
 
 export const menuDataSingleHomePage: MenuItemDataType[] = [
     {
-        title: 'Home',
-        link: '/',
-        megamenu: [
-            {
-                image: '/img/header/home-1.jpg',
-                title: 'Home 01',
-                links: [
-                    { title: 'Multi Page', link: '/' },
-                    { title: 'One Page', link: '/home-one-single' },
-                ],
-            },
-            {
-                image: '/img/header/home-2.jpg',
-                title: 'Home 02',
-                links: [
-                    { title: 'Multi Page', link: 'home-two' },
-                    { title: 'One Page', link: '/home-two-single' },
-                ],
-            },
-            {
-                image: '/img/header/home-3.jpg',
-                title: 'Home 03',
-                links: [
-                    { title: 'Multi Page', link: 'home-three' },
-                    { title: 'One Page', link: '/home-three-single' },
-                ],
-            },
-            {
-                image: '/img/header/home-4.jpg',
-                title: 'Home 04',
-                links: [
-                    { title: 'Multi Page', link: 'home-four' },
-                    { title: 'One Page', link: '/home-four-single' },
-                ],
-            },
-        ],
-    },
-    {
         title: 'About',
         link: '#about',
     },
     {
-        title: 'Services',
+        title: 'Solutions',
         link: '#services',
     },
     {
-        title: 'Projects',
+        title: 'Models',
         link: '#projects',
     },
     {
-        title: 'Team',
-        link: '#team',
+        title: 'Products',
+        link: '#pricing',
     },
     {
         title: 'Blog',
